70976c4ff5e4d451f9a1e369bd8b9bad78a6c134
Eugene Kuntsevich <eugene.kuntsevich@intexsoft.by>
2020-08-30 18:54:49 +0300

    initial commit

---
+++ src/com/company/second_lesson/ArithmeticOperation.java
@@ -0,0 +1,14 @@
+package com.company.second_lesson;
+
+public class ArithmeticOperation
+{
+	public static void main(String[] args)
+	{
+		int i = 1;
+		String s = "string";
+		System.out.println(i + s);
+
+		long aLong2 = 922337203685477580L;
+		i = (int) aLong2;
+	}
+}
---
+++ src/com/company/second_lesson/DataTypes.java
@@ -0,0 +1,26 @@
+package com.company.second_lesson;
+
+public class DataTypes
+{
+	//psvm + Tab, sout + Tab
+	public static void main(String[] args)
+	{
+		boolean isYellow = true; //false
+		Boolean bool = true; //false + null
+
+		int i = 1; //-2147483648 до 2147483647
+		Integer integer = 1; // null
+
+		long along = 1; // –9 223 372 036 854 775 808 до 9 223 372 036 854 775 807
+		Long bigLong = 1L; // null
+
+		String s = "String";
+
+		//non-popular types
+		double aDouble = 8.5;
+		float aFloat = 7.5F;
+		byte b = 1; //-128 до 127
+		short shot = 1; // -32768 до 32767
+		char ch=102; // символ 'f'
+	}
+}
